23fb735faccbc618f1a9ecc56f20d10ba41c979e
Jonas <jonasvargaski@hotmail.com>
2019-07-26 18:49:50 -0300

    desenvolvimento dashboard

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -3,7 +3,7 @@ import { darken } from 'polished';
 
 export const Container = styled.div`
   max-width: 1200px;
-  /* margin: 8px auto; */
+  margin: 8px auto;
   background: #fff;
   border-radius: 4px;
   padding: 5px;
