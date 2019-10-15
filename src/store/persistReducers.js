224bf365e76b9f81ad484d38bfcfbaef74f532f9
Jonas <jonasvargaski@hotmail.com>
2019-10-15 10:33:38 -0300

    upgrade with yarn upgrade

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default reducers => {
   const persistedReducer = persistReducer(
     {
-      key: 'technow',
+      key: 'smartsec:storage',
       storage,
       whitelist: ['auth', 'user'],
     },
